abd97bddbff13327faa66f2fa5a9e97883fd76fc
Pawan Kumar <pwnkumar201@gmail.com>
2025-01-22 17:16:42 +0530

    Counter server side rendering.

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,8 @@
-import React from 'react';
+import React from "react";
 
-import {
-  readFile,
-  writeFile,
-} from '../helpers/file-helpers';
+import { readFile, writeFile } from "../helpers/file-helpers";
 
-const DATABASE_PATH = '/src/database.json';
+const DATABASE_PATH = "/src/database.json";
 
 /*
 `readFile` takes 1 argument:
@@ -24,10 +21,21 @@ writeFile(
 */
 
 function Home() {
+  let { hits } = JSON.parse(readFile(DATABASE_PATH));
+
+  hits += 1;
+
+  writeFile(
+    DATABASE_PATH,
+    JSON.stringify({
+      hits,
+    })
+  );
+
   return (
     <main>
       <h1>Welcome!</h1>
-      <p>You are visitor number X.</p>
+      <p>You are visitor number {hits}.</p>
     </main>
   );
 }
